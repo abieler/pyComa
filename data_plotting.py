8283b08c408f55bd422b365207ef9e8de3ce3f2b
Andre Bieler <andre.bieler81@gmail.com>
2014-03-27 15:55:03 -0400

    more pep8

--- data_plotting.py
+++ data_plotting.py
@@ -7,9 +7,9 @@ from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 
 
-def plotResult(ccd,outputDir,outFileName,instrumentSelector,runDetails,showPlot=False):
-    
-    
+def plot_result(ccd, outputDir, outFileName, instrumentSelector, runDetails,
+                showPlot=False):
+
     if instrumentSelector == 1:
         pltInstrument = 'OSIRIS WAC'
     elif instrumentSelector == 2:
@@ -22,8 +22,7 @@ def plotResult(ccd,outputDir,outFileName,instrumentSelector,runDetails,showPlot=
         pltInstrument = 'VIRTIS M'
     elif instrumentSelector == 6:
         pltInstrument = 'VIRTIS H'
-    
-    
+
     if runDetails.modelCase == 0:
         pltModelCase = 'dsmc model'
     elif runDetails.modelCase == 1:
@@ -34,7 +33,7 @@ def plotResult(ccd,outputDir,outFileName,instrumentSelector,runDetails,showPlot=
         plttpHaser = str(runDetails.tpHaser)
     elif runDetails.modelCase == 2:
         pltModelCase = 'user model'
-        
+
     if runDetails.pointingCase == 0:
         pltPointing = 'spice pointing'
         pltSpiceCase = runDetails.kernelMetaFile
@@ -46,39 +45,42 @@ def plotResult(ccd,outputDir,outFileName,instrumentSelector,runDetails,showPlot=
         pltLatitude = ''
         pltEl = ''
         pltAz = ''
-        
-    
-    pltTitle = '%s, %s, %s\n' %(pltInstrument,pltModelCase, pltPointing)
+
+    pltTitle = '%s, %s, %s\n' % (pltInstrument, pltModelCase, pltPointing)
     if runDetails.pointingCase == 0:
-        pltTitle += '%s, %s\n' %( os.path.split(runDetails.kernelMetaFile)[1], pltUtcStartTime )
+        pltTitle += '%s, %s\n' % (os.path.split(runDetails.kernelMetaFile)[1],
+                                  pltUtcStartTime)
     else:
-        pltTitle += 'R:%i, PA:%i, LAT:%i, a:%i, b:%i, c:%i\n' %(runDetails.userR,runDetails.userPhaseAngle,runDetails.userLatitude,runDetails.userAlpha,runDetails.userBeta, runDetails.userGamma)
-    
+        pltTitle += 'R:%i, PA:%i, LAT:%i, a:%i, b:%i, c:%i\n' %\
+                    (runDetails.userR, runDetails.userPhaseAngle,
+                     runDetails.userLatitude, runDetails.userAlpha,
+                     runDetails.userBeta, runDetails.userGamma)
+
     if runDetails.modelCase == 0:
         pltTitle += '%s\n' % (os.path.split(runDetails.dataFile)[1])
     elif runDetails.modelCase == 1:
-        pltTitle += 'Q: %.2e [#/s],  v: %i [m/s],  Tp: %.0e [s]' %(runDetails.QHaser, runDetails.vHaser, runDetails.tpHaser)
-        
-        if runDetails.tdHaser == None:
+        pltTitle += 'Q: %.2e [#/s],  v: %i [m/s],  Tp: %.0e [s]' % \
+                    (runDetails.QHaser, runDetails.vHaser, runDetails.tpHaser)
+
+        if runDetails.tdHaser is None:
             pltTitle += '\n'
         else:
-            pltTitle += ', Td: %0.e [s]\n' % ( runDetails.tdHaser )
+            pltTitle += ', Td: %0.e [s]\n' % (runDetails.tdHaser)
     elif runDetails.modelCase == 2:
         pass
-    
-    
+
     plt.figure()
     if instrumentSelector == 3:           # alice
-        plt.semilogy(range(5,24),ccd/10**10,'-ok',linewidth=2)
+        plt.semilogy(range(5, 24), ccd / 10**10, '-ok', linewidth=2)
         plt.grid(True)
         plt.xlabel('Pixel Number')
         plt.ylabel('Rayleigh')
-        plt.xticks(range(5,24))
-        plt.xlim((5,23))
+        plt.xticks(range(5, 24))
+        plt.xlim((5, 23))
     else:
-        plt.contourf(ccd,200)
+        plt.contourf(ccd, 200)
         plt.colorbar()
-        
+
     plt.title(pltTitle)
     if showPlot:
         plt.show()
