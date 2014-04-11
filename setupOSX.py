0e20eb00432d907e3722e1a774595980a0d0d819
Andre Bieler <andre.bieler81@gmail.com>
2014-04-11 16:12:29 -0400

    updated docstring

--- setupOSX.py
+++ setupOSX.py
@@ -3,7 +3,8 @@ run the script with:
 
 python setupOSX.py build_ext --inplace
 
-in the same directory as the createRay.pyx file is.
+in the same directory as the createRay.pyx file is. This compiles the cython pyx
+module into createRay.so which can directly imported from python
 '''
 from distutils.core import setup
 from distutils.extension import Extension
