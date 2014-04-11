da26d0b8c50df1a89cbf7452f067cb15a20f04a1
Andre Bieler <andre.bieler81@gmail.com>
2014-04-11 16:09:58 -0400

    initial version

---
+++ setupOSX.py
@@ -0,0 +1,19 @@
+'''
+run the script with:
+
+python setupOSX.py build_ext --inplace
+
+in the same directory as the createRay.pyx file is.
+'''
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [
+            Extension('createRay',
+                      ['createRay.pyx'],
+                      include_dirs=[np.get_include()]
+                      )])
