d015150afeb7e880db4c097403618f0e2df8fd06
Andre Bieler <andre.bieler81@gmail.com>
2014-04-15 15:09:41 -0400

    changed default daughter setting to 0 instead of None

--- haser.py
+++ haser.py
@@ -2,27 +2,27 @@
 from __future__ import division
 import numpy as np
 
-def haserModel(Q, v, tp, td=False):
+
+def haserModel(Q, v, tp, td=0):
     '''
     Q   : gas production rate of parent
     v   : radial outflow velocity parent
     tp  : lifetime parent
     td  : lifetime daughter (if any)
     '''
-    
+
     rmin = np.log10(2000)
     rmax = np.log10(1e8)
-    dr = (rmax - rmin)/ 2000
+    dr = (rmax - rmin) / 2000
     r = 10**(np.arange(rmin, rmax, dr))
-    
+
     gamma_p = v * tp
-    
-    if td == None:
+
+    if td == 0:
         n = Q / (4 * np.pi * r**2 * v) * np.exp(-r/gamma_p)
     else:
         gamma_d = v * td
         n = Q / (4 * np.pi * r**2 * v) * (gamma_d / (gamma_p - gamma_d)) * (np.exp(-r/gamma_p) - np.exp(-r/gamma_d))
-    
     return r, n
 
 
