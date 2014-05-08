42870b4cf270deb7fc7e1bbd9f2cb12f68f6b172
Andre Bieler <andre.bieler81@gmail.com>
2014-05-08 14:35:35 -0400

    changed default output dir

--- utils/cmdline_args.py
+++ utils/cmdline_args.py
@@ -10,7 +10,7 @@ def cmdline_args(parser):
     parser.add_argument("--iPointingCase", type=int, choices=[0, 1, 2],
                         default=1, help='0: spice pointing, 1: user pointing, 2: user trajectory')
     parser.add_argument("--iInstrumentSelector", type=int, choices=[1, 2, 3, 4, 5, 6], default=1)
-    parser.add_argument("--StringOutputDir", type=str, default='.')
+    parser.add_argument("--StringOutputDir", type=str, default='results')
 
     parser.add_argument("--StringDSMCdir", type=str)
     parser.add_argument("--StringDataFileDSMC", type=str)
