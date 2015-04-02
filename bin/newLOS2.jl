bfe150962c750d5dd40c74dfedb3cd35addf8171
abieler <andre.bieler81@gmail.com>
2015-04-02 15:49:25 -0400

    changed reading of file name extension

--- bin/newLOS2.jl
+++ bin/newLOS2.jl
@@ -2,12 +2,14 @@ using HDF5, JLD
 include("types2.jl")
 
 const fileName = ARGS[1]
-const pFileName = ARGS[2]
+#const pFileName = ARGS[2]
+const pFileName = ARGS[2] * "/pointing.dat"
 const filePath = dirname(ARGS[1])
 
 # subtract extension and last dot from filename
-fileNameExtension = split(basename(fileName), ".")
-fileNameBase = basename(fileName)[1:end-(length(fileNameExtension)+2)]
+fileNameExtension = split(basename(fileName), ".")[end]
+fileNameBase = basename(fileName)[1:end-(length(fileNameExtension)+1)]
+println("fileNameBase: ", fileNameBase)
 @load joinpath(filePath,  fileNameBase * "_nodes.jld")
 @load joinpath(filePath, fileNameBase * "_nodeCoordinates.jld")
 @load joinpath(filePath, fileNameBase * "_cubeIndices.jld") 
@@ -21,6 +23,7 @@ const yMax = maximum(nodes[:,:,2])
 const zMax = maximum(nodes[:,:,3])
 
 const halfSize = [xMax, yMax, zMax]
+println("halfSize: ", halfSize)
 const root = [0.0, 0.0, 0.0]
 
 @time cellList = build_cells(nodes, cubeIndices, numberDensity, nCells)
@@ -28,3 +31,5 @@ blocks = build_blocks(nBlocks, cellList, nodes)
 octree = Block(root, halfSize,1, initChildren, initCells)
 println("populating octree")
 populate_octree(octree, blocks, nBlocks)
+n = doIntegration(octree, pFileName)
+writedlm(ARGS[2]* "/ccd.dat", n)
