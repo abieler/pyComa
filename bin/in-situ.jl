d4a67736b543ce56f02bcea011d13e6c64219a37
Jeff Kopmanis <kopmanis@umich.edu>
2015-05-19 10:37:56 -0400

    Added in-situ.jl to pyComa from ~abeiler

---
+++ bin/in-situ.jl
@@ -0,0 +1,38 @@
+println("start julia in-situ calculation. not finished yet")
+using HDF5
+using JLD
+include("types.jl")
+
+
+nDim = 3
+path = ARGS[1]
+pFileName = ARGS[2] * "/rosettaCoords.txt"
+
+println("dsmc path: ", path)
+DSMCfileNames = readdir(path)
+
+fileName = ""
+for f in DSMCfileNames
+    println(f)
+    if contains(f, "H2O")
+        fileName = path * f
+    end
+end
+
+halfSize = [200000., 200000., 200000.]
+root = [0.0, 0.0, 0.0]
+
+nCells, nCellsPerBlock, nBlocks, nodeCoordinates, cubeIndices, numberDensity = load_AMPS_data_full(fileName)
+cellList, nodes = build_cells(nodeCoordinates, cubeIndices, nCells, numberDensity)
+blocks = build_blocks(nBlocks, cellList, nodes)
+
+octree = Block(root, halfSize,1, initChildren, initCells)
+println("populating octree")
+populate_octree(octree, blocks, nBlocks)
+n = doInSituCalculation(octree, pFileName)
+println(" - write result to file")
+println(size(n))
+writedlm(ARGS[2] * "/interpolation.out", n)
+println(" - write result to file ok")
+println(" - done julia interpolation")
+println("+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +")
